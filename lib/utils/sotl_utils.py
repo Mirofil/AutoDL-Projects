5a7f96f1d7e938c34bf1378dc66303f8c872dd89
Mirofil <kawgawr@gmail.com>
2021-02-06 13:48:03 +0100

    Gdrive WANDB auth

--- lib/utils/sotl_utils.py
+++ lib/utils/sotl_utils.py
@@ -142,31 +142,37 @@ def calculate_valid_accs(xloader, archs, network):
   return valid_accs
 
 def wandb_auth(fname: str = "nas_key.txt"):
-    if "WANDB_API_KEY" in os.environ:
-        wandb_key = os.environ["WANDB_API_KEY"]
-    elif os.path.exists(os.path.abspath("~" + os.sep + ".wandb" + os.sep + fname)):
-        # This branch does not seem to work as expected on Paperspace - it gives '/storage/~/.wandb/nas_key.txt'
-        print("Retrieving WANDB key from file")
-        f = open("~" + os.sep + ".wandb" + os.sep + fname, "r")
-        key = f.read().strip()
-        os.environ["WANDB_API_KEY"] = key
-    elif os.path.exists("/root/.wandb/"+fname):
-        print("Retrieving WANDB key from file")
-        f = open("/root/.wandb/"+fname, "r")
-        key = f.read().strip()
-        os.environ["WANDB_API_KEY"] = key
+  gdrive_path = "/content/drive/MyDrive/Colab Notebooks/wandb/nas_key.txt"
+  if "WANDB_API_KEY" in os.environ:
+      wandb_key = os.environ["WANDB_API_KEY"]
+  elif os.path.exists(os.path.abspath("~" + os.sep + ".wandb" + os.sep + fname)):
+      # This branch does not seem to work as expected on Paperspace - it gives '/storage/~/.wandb/nas_key.txt'
+      print("Retrieving WANDB key from file")
+      f = open("~" + os.sep + ".wandb" + os.sep + fname, "r")
+      key = f.read().strip()
+      os.environ["WANDB_API_KEY"] = key
+  elif os.path.exists("/root/.wandb/"+fname):
+      print("Retrieving WANDB key from file")
+      f = open("/root/.wandb/"+fname, "r")
+      key = f.read().strip()
+      os.environ["WANDB_API_KEY"] = key
 
-    elif os.path.exists(
-        os.path.expandvars("%userprofile%") + os.sep + ".wandb" + os.sep + fname
-    ):
-        print("Retrieving WANDB key from file")
-        f = open(
-            os.path.expandvars("%userprofile%") + os.sep + ".wandb" + os.sep + fname,
-            "r",
-        )
-        key = f.read().strip()
-        os.environ["WANDB_API_KEY"] = key
-    wandb.login()
+  elif os.path.exists(
+      os.path.expandvars("%userprofile%") + os.sep + ".wandb" + os.sep + fname
+  ):
+      print("Retrieving WANDB key from file")
+      f = open(
+          os.path.expandvars("%userprofile%") + os.sep + ".wandb" + os.sep + fname,
+          "r",
+      )
+      key = f.read().strip()
+      os.environ["WANDB_API_KEY"] = key
+  elif os.path.exists(gdrive_path):
+      print("Retrieving WANDB key from file")
+      f = open(gdrive, "r")
+      key = f.read().strip()
+      os.environ["WANDB_API_KEY"] = key
+  wandb.login()
 
 
 def simulate_train_eval_sotl(
