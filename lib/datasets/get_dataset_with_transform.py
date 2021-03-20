b7c66c482a4e584da403ead4a4587b2552c24dc6
Mirofil <kawgawr@gmail.com>
2021-03-20 13:19:02 +0100

    Add back shuffling

--- lib/datasets/get_dataset_with_transform.py
+++ lib/datasets/get_dataset_with_transform.py
@@ -313,9 +313,9 @@ def get_nas_search_loaders(train_data, valid_data, dataset, config_root, batch_s
     search_loader = torch.utils.data.DataLoader(search_data, batch_size=batch, sampler=torch.utils.data.sampler.SubsetRandomSampler(train_split) if determinism not in ['train', 'all'] else SubsetSequentialSampler(indices=train_split, epochs=epochs, extra_split=True, shuffle=False),
        num_workers=workers, pin_memory=True)
     train_loader  = torch.utils.data.DataLoader(train_data , batch_size=batch, 
-      sampler=torch.utils.data.sampler.SubsetRandomSampler(train_split) if determinism not in ['train', 'all'] else SubsetSequentialSampler(indices=train_split, epochs=epochs, extra_split=True), num_workers=workers, pin_memory=True)
+      sampler=torch.utils.data.sampler.SubsetRandomSampler(train_split) if determinism not in ['train', 'all'] else SubsetSequentialSampler(indices=train_split, epochs=epochs, extra_split=True, shuffle=False), num_workers=workers, pin_memory=True)
     valid_loader  = torch.utils.data.DataLoader(train_data, batch_size=test_batch, 
-      sampler=torch.utils.data.sampler.SubsetRandomSampler(valid_split) if determinism not in ['val', 'all'] else SubsetSequentialSampler(indices=valid_split, epochs=epochs, extra_split=True), num_workers=workers, pin_memory=True)
+      sampler=torch.utils.data.sampler.SubsetRandomSampler(valid_split) if determinism not in ['val', 'all'] else SubsetSequentialSampler(indices=valid_split, epochs=epochs, extra_split=True, shuffle=False), num_workers=workers, pin_memory=True)
   elif dataset == 'cifar100':
     cifar100_test_split = load_config('{:}/cifar100-test-split.txt'.format(config_root), None, None)
     search_train_data = train_data
