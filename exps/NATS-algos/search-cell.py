f9bc20492474ac8732f5bce6bfe26687f4ac4bfc
Mirofil <kawgawr@gmail.com>
2021-03-23 08:58:05 +0100

    Whats going on with the checkpoints good good

--- exps/NATS-algos/search-cell.py
+++ exps/NATS-algos/search-cell.py
@@ -372,9 +372,9 @@ def get_best_arch(train_loader, valid_loader, network, n_samples, algo, logger,
       checkpoint_config = checkpoint["config"] if "config" in checkpoint.keys() else {}
 
       try:
-        if type(list(checkpoint["metrics"]["sotl"].keys())[0]) is not str or type(checkpoint["metrics"]) is dict:
-          must_restart = True # will need to restart metrics because using the old checkpoint format
-          print("Using old checkpoint format! Must restart")
+        # if type(list(checkpoint["metrics"]["sotl"].keys())[0]) is not str or type(checkpoint["metrics"]) is dict:
+        #   must_restart = True # will need to restart metrics because using the old checkpoint format
+        #   print("Using old checkpoint format! Must restart")
         metrics = {k:checkpoint["metrics"][k] for k in checkpoint["metrics"].keys()}
 
         # prototype = metrics[metrics_keys[0]]
