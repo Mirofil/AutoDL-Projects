c7f06de592a351cd67f59ba4184085f818bb1aa5
Mirofil <kawgawr@gmail.com>
2021-04-14 12:44:08 +0200

    Fix bug

--- lib/utils/sotl_utils.py
+++ lib/utils/sotl_utils.py
@@ -61,8 +61,8 @@ def arch_percentiles(arch_dict=None, percentiles = [0, 25, 50, 75, 100]):
   arch_list = sorted(arch_list, key=lambda x: x[1]) # Highest values are last
   percentiles_dict = {}
   for i in range(len(percentiles)-1):
-    for arch in arch_list[round(len(arch_list)*(percentiles[i]/100)):round(len(arch_list)*(percentiles[i+1]/100))]:
-      percentiles_dict[arch] = percentiles[i+1]
+    for arch_tuple in arch_list[round(len(arch_list)*(percentiles[i]/100)):round(len(arch_list)*(percentiles[i+1]/100))]:
+      percentiles_dict[arch_tuple[0]] = percentiles[i+1]
   print(list(percentiles_dict.items())[0:10])
   return percentiles_dict
 
