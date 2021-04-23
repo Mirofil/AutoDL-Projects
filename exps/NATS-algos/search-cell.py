94e8a47f0a36468b7b5934a2503d53b33b2441f1
Mirofil <kawgawr@gmail.com>
2021-04-23 11:25:44 +0100

    Fixed upper boudnd for real'

--- exps/NATS-algos/search-cell.py
+++ exps/NATS-algos/search-cell.py
@@ -500,10 +500,11 @@ def get_best_arch(train_loader, valid_loader, network, n_samples, algo, logger,
           archs = checkpoint["archs"]
           true_rankings, final_accs = get_true_rankings(archs, api)
           upper_bound = {}
-          for n, dataset in zip([1,5, 10], true_rankings.keys()):
+          for n in [1,5,10]:
             upper_bound[f"top{n}"] = {"cifar10":0, "cifar10-valid":0, "cifar100":0, "ImageNet16-120":0}
-            upper_bound[f"top{n}"][dataset] += sum([x["metric"] for x in true_rankings[dataset][0:n]])/min(n, len(true_rankings[dataset][0:n]))
-            # upper_bound["top1"][dataset] += sum([x["metric"] for x in true_rankings[dataset][0:1]])/1
+            for dataset in true_rankings.keys():
+              upper_bound[f"top{n}"][dataset] += sum([x["metric"] for x in true_rankings[dataset][0:n]])/min(n, len(true_rankings[dataset][0:n]))
+              # upper_bound["top1"][dataset] += sum([x["metric"] for x in true_rankings[dataset][0:1]])/1
           upper_bound = {"upper":upper_bound}
 
     if xargs.restart:
