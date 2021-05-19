5c247b72649bab7d01eb31bd720ed800f114e1ea
Mirofil <kawgawr@gmail.com>
2021-05-19 08:31:43 +0100

    Some more debug messages

--- exps/NATS-algos/search-cell.py
+++ exps/NATS-algos/search-cell.py
@@ -336,6 +336,7 @@ def search_func(xloader, network, criterion, scheduler, w_optimizer, a_optimizer
     inner_steps = args.inner_steps
   else:
     inner_steps = 1 # SPOS equivalent
+  logger.log(f"Starting search with batch_size={len(next(iter(xloader)))}, len={len(xloader)}")
   for step, (base_inputs, base_targets, arch_inputs, arch_targets) in tqdm(enumerate(search_loader_iter), desc = "Iterating over SearchDataset", total = round(len(xloader)/(inner_steps if not args.inner_steps_same_batch else 1))): # Accumulate gradients over backward for sandwich rule
     all_base_inputs, all_base_targets, all_arch_inputs, all_arch_targets = format_input_data(base_inputs, base_targets, arch_inputs, arch_targets, search_loader_iter, inner_steps, args)
 
@@ -791,53 +792,54 @@ def search_func(xloader, network, criterion, scheduler, w_optimizer, a_optimizer
       if step == print_freq:
         logger.log(network.alphas)
 
-  if args.hessian and algo.startswith('darts') and torch.cuda.get_device_properties(0).total_memory > (20147483648 if args.max_nodes < 7 else 20147483648): # Crashes with just 8GB of memory
-    labels = []
-    for i in range(network._max_nodes):
-      for n in network._op_names:
-        labels.append(n + "_" + str(i))
+  if False:
+    if args.hessian and algo.startswith('darts') and torch.cuda.get_device_properties(0).total_memory > (20147483648 if args.max_nodes < 7 else 20147483648): # Crashes with just 8GB of memory
+      labels = []
+      for i in range(network._max_nodes):
+        for n in network._op_names:
+          labels.append(n + "_" + str(i))
 
-    network.logits_only=True
-    val_x, val_y = next(iter(val_loader))
-    val_loss = criterion(network(val_x.to('cuda')), val_y.to('cuda'))
-    train_x, train_y, _, _ = next(iter(xloader))
-    train_loss = criterion(network(train_x.to('cuda')), train_y.to('cuda'))
-    val_hessian_mat = _hessian(val_loss, network.arch_params())
-    if epoch == 0:
-      logger.log(f"Example architecture Hessian: {val_hessian_mat}")
-    val_eigenvals, val_eigenvecs = torch.eig(val_hessian_mat)
-    if not args.merge_train_val_supernet:
-      train_hessian_mat = _hessian(train_loss, network.arch_params())
-      train_eigenvals, train_eigenvecs = torch.eig(train_hessian_mat)
-    else:
-      train_eigenvals = val_eigenvals
-    val_eigenvals = val_eigenvals[:, 0] # Drop the imaginary components
-    if epoch == 0:
-      logger.log(f"Example architecture eigenvals: {val_eigenvals}")
-    train_eigenvals = train_eigenvals[:, 0]
-    val_dom_eigenvalue = torch.max(val_eigenvals)
-    train_dom_eigenvalue = torch.max(train_eigenvals)
-    eigenvalues = {"max":{}, "spectrum": {}}
-    eigenvalues["max"]["train"] = train_dom_eigenvalue
-    eigenvalues["max"]["val"] = val_dom_eigenvalue
-    eigenvalues["spectrum"]["train"] = {k:v for k,v in zip(labels, train_eigenvals)}
-    eigenvalues["spectrum"]["val"] = {k:v for k,v in zip(labels, val_eigenvals)}
-    network.logits_only=False
+      network.logits_only=True
+      val_x, val_y = next(iter(val_loader))
+      val_loss = criterion(network(val_x.to('cuda')), val_y.to('cuda'))
+      train_x, train_y, _, _ = next(iter(xloader))
+      train_loss = criterion(network(train_x.to('cuda')), train_y.to('cuda'))
+      val_hessian_mat = _hessian(val_loss, network.arch_params())
+      if epoch == 0:
+        logger.log(f"Example architecture Hessian: {val_hessian_mat}")
+      val_eigenvals, val_eigenvecs = torch.eig(val_hessian_mat)
+      if not args.merge_train_val_supernet:
+        train_hessian_mat = _hessian(train_loss, network.arch_params())
+        train_eigenvals, train_eigenvecs = torch.eig(train_hessian_mat)
+      else:
+        train_eigenvals = val_eigenvals
+      val_eigenvals = val_eigenvals[:, 0] # Drop the imaginary components
+      if epoch == 0:
+        logger.log(f"Example architecture eigenvals: {val_eigenvals}")
+      train_eigenvals = train_eigenvals[:, 0]
+      val_dom_eigenvalue = torch.max(val_eigenvals)
+      train_dom_eigenvalue = torch.max(train_eigenvals)
+      eigenvalues = {"max":{}, "spectrum": {}}
+      eigenvalues["max"]["train"] = train_dom_eigenvalue
+      eigenvalues["max"]["val"] = val_dom_eigenvalue
+      eigenvalues["spectrum"]["train"] = {k:v for k,v in zip(labels, train_eigenvals)}
+      eigenvalues["spectrum"]["val"] = {k:v for k,v in zip(labels, val_eigenvals)}
+      network.logits_only=False
 
-  elif args.hessian and algo.startswith('darts') and torch.cuda.get_device_properties(0).total_memory < 9147483648:
-    network.logits_only=True
-    val_eigenvals, val_eigenvals = compute_hessian_eigenthings(network, val_loader, criterion, 1, mode="power_iter", power_iter_steps=50, max_samples=128, arch_only=True, full_dataset=False)
-    val_dom_eigenvalue = val_eigenvals[0]
-    if not args.merge_train_val_supernet:
-      train_eigenvals, train_eigenvecs = compute_hessian_eigenthings(network, xloader, criterion, 1, mode="power_iter", power_iter_steps=50, max_samples=128, arch_only=True, full_dataset=False)
-      train_dom_eigenvalue = train_eigenvals[0]
-    else:
-      train_eigenvals, train_eigenvecs = None, None
-      train_dom_eigenvalue = None
-    eigenvalues = {"max":{}, "spectrum": {}}
-    eigenvalues["max"]["val"] = val_dom_eigenvalue
-    eigenvalues["max"]["train"] = train_dom_eigenvalue
-    network.logits_only=False
+    elif args.hessian and algo.startswith('darts') and torch.cuda.get_device_properties(0).total_memory < 9147483648:
+      network.logits_only=True
+      val_eigenvals, val_eigenvals = compute_hessian_eigenthings(network, val_loader, criterion, 1, mode="power_iter", power_iter_steps=50, max_samples=128, arch_only=True, full_dataset=False)
+      val_dom_eigenvalue = val_eigenvals[0]
+      if not args.merge_train_val_supernet:
+        train_eigenvals, train_eigenvecs = compute_hessian_eigenthings(network, xloader, criterion, 1, mode="power_iter", power_iter_steps=50, max_samples=128, arch_only=True, full_dataset=False)
+        train_dom_eigenvalue = train_eigenvals[0]
+      else:
+        train_eigenvals, train_eigenvecs = None, None
+        train_dom_eigenvalue = None
+      eigenvalues = {"max":{}, "spectrum": {}}
+      eigenvalues["max"]["val"] = val_dom_eigenvalue
+      eigenvalues["max"]["train"] = train_dom_eigenvalue
+      network.logits_only=False
 
   else:
     eigenvalues = None
