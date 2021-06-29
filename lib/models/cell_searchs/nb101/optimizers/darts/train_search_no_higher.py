42357b7c782b2fb1f71e93558d2a02656f6c4544
Mirofil <kawgawr@gmail.com>
2021-06-29 22:30:39 +0100

    lets go with SDARTS and see what happens

--- lib/models/cell_searchs/nb101/optimizers/darts/train_search_no_higher.py
+++ lib/models/cell_searchs/nb101/optimizers/darts/train_search_no_higher.py
@@ -247,6 +247,7 @@ def main():
         print(f"Genotype performance: {genotype_perf}" )
         if args.hessian:
             eigenvalues = approx_hessian(network=model, val_loader=valid_queue, criterion=criterion, xloader=valid_queue, args=args)
+            print(f"Eigenvalues: {eigenvalues}")
             # eigenvalues = exact_hessian(network=model, val_loader=valid_queue, criterion=criterion, xloader=valid_queue, epoch=epoch, logger=logger, args=args)
         else:
             eigenvalues = None
--- lib/utils/train_loop.py
+++ lib/utils/train_loop.py
@@ -890,7 +890,7 @@ def approx_hessian(network, val_loader, criterion, xloader, args):
                                                              power_iter_steps=50, max_samples=128, arch_only=True, full_dataset=False)
   val_dom_eigenvalue = val_eigenvals[0]
   try:
-    if not args.merge_train_val_supernet:
+    if hasattr(args, merge_train_val_supernet) and not args.merge_train_val_supernet:
       train_eigenvals, train_eigenvecs = compute_hessian_eigenthings(network, val_loader, criterion, 1, mode="power_iter", 
                                                                     power_iter_steps=50, max_samples=128, arch_only=True, full_dataset=False)
       train_dom_eigenvalue = train_eigenvals[0]
