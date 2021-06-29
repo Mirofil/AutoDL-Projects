123d515bec0f72aa6de9b284c25b176bb5d400eb
Mirofil <kawgawr@gmail.com>
2021-06-29 10:15:03 +0100

    Try second order DARTS yo

--- lib/models/cell_searchs/nb101/optimizers/darts/train_search_higher.py
+++ lib/models/cell_searchs/nb101/optimizers/darts/train_search_higher.py
@@ -1,3 +1,6 @@
+# python lib/models/cell_searchs/nb101/optimizers/darts/train_search_higher.py --batch_size=32 --seed=999 --higher_method=sotl --higher_params=arch --higher_order=second
+
+
 import argparse
 import glob
 import json
@@ -26,7 +29,8 @@ from optimizers.darts import utils
 from optimizers.darts.architect import Architect
 from optimizers.darts.model_search import Network
 
-from sotl_utils import wandb_auth
+from sotl_utils import format_input_data, fo_grad_if_possible, hyper_meta_step, hypergrad_outer
+
 import wandb
 from pathlib import Path
 from tqdm import tqdm
@@ -96,7 +100,39 @@ logging.getLogger().addHandler(fh)
 
 CIFAR_CLASSES = 10
 
+def wandb_auth(fname: str = "nas_key.txt"):
+  gdrive_path = "/content/drive/MyDrive/colab/wandb/nas_key.txt"
+  if "WANDB_API_KEY" in os.environ:
+      wandb_key = os.environ["WANDB_API_KEY"]
+  elif os.path.exists(os.path.abspath("~" + os.sep + ".wandb" + os.sep + fname)):
+      # This branch does not seem to work as expected on Paperspace - it gives '/storage/~/.wandb/nas_key.txt'
+      print("Retrieving WANDB key from file")
+      f = open("~" + os.sep + ".wandb" + os.sep + fname, "r")
+      key = f.read().strip()
+      os.environ["WANDB_API_KEY"] = key
+  elif os.path.exists("/root/.wandb/"+fname):
+      print("Retrieving WANDB key from file")
+      f = open("/root/.wandb/"+fname, "r")
+      key = f.read().strip()
+      os.environ["WANDB_API_KEY"] = key
 
+  elif os.path.exists(
+      os.path.expandvars("%userprofile%") + os.sep + ".wandb" + os.sep + fname
+  ):
+      print("Retrieving WANDB key from file")
+      f = open(
+          os.path.expandvars("%userprofile%") + os.sep + ".wandb" + os.sep + fname,
+          "r",
+      )
+      key = f.read().strip()
+      os.environ["WANDB_API_KEY"] = key
+  elif os.path.exists(gdrive_path):
+      print("Retrieving WANDB key from file")
+      f = open(gdrive_path, "r")
+      key = f.read().strip()
+      os.environ["WANDB_API_KEY"] = key
+  wandb.login()
+  
 def main():
     # Select the search space to search in
     if args.search_space == '1':
@@ -195,7 +231,8 @@ def main():
         logging.info(f'architecture : {numpy_tensor_list}')
 
         # training
-        train_acc, train_obj = train(train_queue, valid_queue, model, architect, criterion, optimizer, lr, epoch)
+        train_acc, train_obj = train(train_queue=train_queue, valid_queue=valid_queue, network=model, architect=architect, criterion=criterion, 
+                                     w_optimizer=optimizer, a_optimizer=architect.optimizer, epoch=epoch)
         logging.info('train_acc %f', train_acc)
 
         # validation
@@ -221,52 +258,109 @@ def main():
     for log in tqdm(all_logs, desc = "Logging search logs"):
         wandb.log(log)
 
-def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr, epoch):
+
+def train(train_queue, valid_queue, network, architect, criterion, w_optimizer, a_optimizer, logger=None, inner_steps=100, epoch=0, steps_per_epoch=None):
+    import higher
+    
     objs = utils.AvgrageMeter()
     top1 = utils.AvgrageMeter()
     top5 = utils.AvgrageMeter()
 
-    for step, (input, target) in enumerate(train_queue):
-        model.train()
-        n = input.size(0)
+    train_iter = iter(train_queue)
+    valid_iter = iter(valid_queue)
+    search_loader_iter = zip(train_iter, valid_iter)
+    for data_step, ((base_inputs, base_targets), (arch_inputs, arch_targets)) in tqdm(enumerate(search_loader_iter), total = round(len(train_queue)/inner_steps)):
+      if steps_per_epoch is not None and data_step > steps_per_epoch:
+        break
+      network.train()
+      n = base_inputs.size(0)
 
-        input = input.cuda()
-        target = target.cuda(non_blocking=True)
+      base_inputs = base_inputs.cuda()
+      base_targets = base_targets.cuda(non_blocking=True)
 
-        # get a minibatch from the search queue with replacement
-        try:
-            input_search, target_search = next(valid_queue_iter)
-        except:
-            valid_queue_iter = iter(valid_queue)
-            input_search, target_search = next(valid_queue_iter)
+      # get a random minibatch from the search queue with replacement
+      input_search, target_search = next(iter(valid_queue))
+      input_search = input_search.cuda()
+      target_search = target_search.cuda(non_blocking=True)
+      
+      all_base_inputs, all_base_targets, all_arch_inputs, all_arch_targets = format_input_data(base_inputs, base_targets, arch_inputs, arch_targets, 
+                                                                                                search_loader_iter, inner_steps=100, args=args)
+      weights_mask = [1 if ('arch' not in n and 'alpha' not in n) else 0 for (n, p) in network.named_parameters()] # Zeroes out all the architecture gradients in Higher. It has to be hacked around like this due to limitations of the library
+      zero_arch_grads = lambda grads: [g*x if g is not None else None for g,x in zip(grads, weights_mask)]
+      monkeypatch_higher_grads_cond = True if (args.meta_algo not in ['reptile', 'metaprox'] and (args.higher_order != "first" or args.higher_method == "val")) else False
+      diffopt_higher_grads_cond = True if (args.meta_algo not in ['reptile', 'metaprox'] and args.higher_order != "first") else False
+      fnetwork = higher.patch.monkeypatch(network, device='cuda', copy_initial_weights=True if args.higher_loop == "bilevel" else False, track_higher_grads = monkeypatch_higher_grads_cond)
+      diffopt = higher.optim.get_diff_optim(w_optimizer, network.parameters(), fmodel=fnetwork, grad_callback=zero_arch_grads, device='cuda', override=None, track_higher_grads = diffopt_higher_grads_cond) 
+      fnetwork.zero_grad() # TODO where to put this zero_grad? was there below in the sandwich_computation=serial branch, tbut that is surely wrong since it wouldnt support higher meta batch size
+      
+      sotl, first_order_grad = [], None
+      inner_rollouts, meta_grads = [], [] # For implementing meta-batch_size in Reptile/MetaProx and similar
 
-        input_search = input_search.cuda()
-        target_search = target_search.cuda(non_blocking=True)
+      for inner_step, (base_inputs, base_targets, arch_inputs, arch_targets) in enumerate(zip(all_base_inputs, all_base_targets, all_arch_inputs, all_arch_targets)):
+          if data_step in [0, 1] and inner_step < 3:
+              print(f"Base targets in the inner loop at inner_step={inner_step}, step={data_step}: {base_targets[0:10]}")
+          logits = fnetwork(base_inputs)
+          base_loss = criterion(logits, base_targets)
+          sotl.append(base_loss)
+          new_params, cur_grads = diffopt.step(base_loss)
+          cur_grads = list(cur_grads)
+          for idx, (g, p) in enumerate(zip(cur_grads, fnetwork.parameters())):
+              if g is None:
+                  cur_grads[idx] = torch.zeros_like(p)
+          first_order_grad_for_free_cond = False
+          first_order_grad_concurrently_cond = False
+          first_order_grad = fo_grad_if_possible(args, fnetwork, criterion, all_arch_inputs, all_arch_targets, arch_inputs, arch_targets, cur_grads, inner_step, 
+                                                  data_step, 1, first_order_grad, first_order_grad_for_free_cond, first_order_grad_concurrently_cond, logger=None)
+      meta_grads, inner_rollouts = hypergrad_outer(args=args, fnetwork=fnetwork, criterion=criterion, arch_targets=arch_targets, arch_inputs=arch_inputs,
+                                          all_arch_inputs=all_arch_inputs, all_arch_targets=all_arch_targets, all_base_inputs=all_base_inputs, all_base_targets=all_base_targets,
+                                          sotl=sotl, inner_step=inner_step, inner_steps=inner_steps, inner_rollouts=inner_rollouts,
+                                          first_order_grad_for_free_cond=first_order_grad_for_free_cond, first_order_grad_concurrently_cond=first_order_grad_concurrently_cond,
+                                          monkeypatch_higher_grads_cond=monkeypatch_higher_grads_cond, zero_arch_grads_lambda=zero_arch_grads, meta_grads=meta_grads,
+                                          step=data_step, epoch=epoch, logger=None)
+  
+      if first_order_grad is not None:
+          assert first_order_grad_for_free_cond or first_order_grad_concurrently_cond
+          if epoch < 2:
+              print(f"Putting first_order_grad into meta_grads (NOTE we aggregate first order grad by summing in the first place to save memory, so dividing by inner steps gives makes it average over the rollout) (len of first_order_grad ={len(first_order_grad)}, len of param list={len(list(network.parameters()))}) with reduction={args.higher_reduction}, inner_steps (which is the division factor)={inner_steps}, head={first_order_grad[0]}")
+          if args.higher_reduction == "sum": # the first_order_grad is computed in a way that equals summing
+              meta_grads.append(first_order_grad)
+          else:
+              meta_grads.append([g/inner_steps if g is not None else g for g in first_order_grad])  
+              
+      avg_meta_grad = hyper_meta_step(network, inner_rollouts, meta_grads, args, data_step, logger, model_init=None, outer_iters=1, epoch=epoch)
+      with torch.no_grad():  # Update the pre-rollout weights
+          for (n, p), g in zip(network.named_parameters(), avg_meta_grad):
+              cond = ('arch' not in n and 'alpha' not in n) if args.higher_params == "weights" else ('arch' in n or 'alpha' in n)  # The meta grads typically contain all gradient params because they arise as a result of torch.autograd.grad(..., model.parameters()) in Higher
+              if cond:
+                  if g is not None and p.requires_grad:
+                      p.grad = g
+      a_optimizer.step()
+      
+      w_optimizer.zero_grad()
+      architect.optimizer.zero_grad()
+      
+      for inner_step, (base_inputs, base_targets, arch_inputs, arch_targets) in enumerate(zip(all_base_inputs, all_base_targets, all_arch_inputs, all_arch_targets)):
+          if data_step in [0, 1] and inner_step < 3 and epoch % 5 == 0:
+              logger.info(f"Doing weight training for real in higher_loop={args.higher_loop} at inner_step={inner_step}, step={data_step}: {base_targets[0:10]}")
+          logits = network(base_inputs)
+          base_loss = criterion(logits, base_targets)
+          network.zero_grad()
+          base_loss.backward()
+          w_optimizer.step()
+          n = base_inputs.size(0)
 
-        # Allow for warm starting of the one-shot model for more reliable architecture updates.
-        if epoch >= args.warm_start_epochs:
-            architect.step(input, target, input_search, target_search, lr, optimizer, unrolled=args.unrolled)
+          prec1, prec5 = utils.accuracy(logits, base_targets, topk=(1, 5))
 
-        optimizer.zero_grad()
-        logits = model(input)
-        loss = criterion(logits, target)
+          objs.update(base_loss.item(), n)
+          top1.update(prec1.data, n)
+          top5.update(prec5.data, n)
 
-        loss.backward()
-        nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
-        optimizer.step()
+      if data_step % args.report_freq == 0:
+          logging.info('train %03d %e %f %f', data_step, objs.avg, top1.avg, top5.avg)
+      if 'debug' in args.save:
+          break
 
-        prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-        objs.update(loss.data.item(), n)
-        top1.update(prec1.data.item(), n)
-        top5.update(prec5.data.item(), n)
-
-        if step % args.report_freq == 0:
-            logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
-            if args.debug:
-                break
-
-    return top1.avg, objs.avg
-
+    return  top1.avg, objs.avg
 
 def infer(valid_queue, model, criterion):
     objs = utils.AvgrageMeter()
