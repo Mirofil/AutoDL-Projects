5c2a394e7a6ff5159265948d8558f82a1944ccee
Mirofil <kawgawr@gmail.com>
2021-07-03 16:04:46 +0100

    Wahat went wrong iwth the checkpointing meeh

--- lib/models/cell_searchs/darts/cnn/train_search_no_higher.py
+++ lib/models/cell_searchs/darts/cnn/train_search_no_higher.py
@@ -75,7 +75,7 @@ parser.add_argument('--meta_algo' ,       type=str, choices=['reptile', 'metapro
 
 args = parser.parse_args()
 
-args.save = 'darts_output/search-higher-{}-{}-{}'.format(args.save, args.unrolled, args.seed)
+args.save = 'darts_output/search-no_higher-{}-{}-{}'.format(args.save, args.unrolled, args.seed)
 try:
   utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
 except Exception as e:
