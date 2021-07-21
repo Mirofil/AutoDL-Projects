c793b94814c4906de58fb3d8c58ee14fd4a5f441
Mirofil <kawgawr@gmail.com>
2021-07-21 18:49:39 +0100

    Lets go

--- lib/models/cell_searchs/nb101/optimizers/darts/train_search_higher.py
+++ lib/models/cell_searchs/nb101/optimizers/darts/train_search_higher.py
@@ -359,7 +359,7 @@ def train(train_queue, valid_queue, network, architect, criterion, w_optimizer, 
     train_iter = iter(train_queue)
     valid_iter = iter(valid_queue)
     search_loader_iter = zip(train_iter, valid_iter)
-    for data_step, ((base_inputs, base_targets), (arch_inputs, arch_targets)) in tqdm(enumerate(search_loader_iter), total = round(len(train_queue)/inner_steps), disable = False):
+    for data_step, ((base_inputs, base_targets), (arch_inputs, arch_targets)) in tqdm(enumerate(search_loader_iter), total = round(len(train_queue)/inner_steps), disable = True if len(train_queue)/inner_steps>90 else False):
       if steps_per_epoch is not None and data_step > steps_per_epoch:
         break
       network.train()
